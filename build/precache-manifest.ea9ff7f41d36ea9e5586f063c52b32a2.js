cec1983a27ac8d00c3831e2363e5c464cf69f13c
anshu gupta <anshu.snm6@gmail.com>
2020-12-20 20:56:10 +0530

    fist entire commit

---
+++ build/precache-manifest.ea9ff7f41d36ea9e5586f063c52b32a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd84508e0285cbdee96c41754a443ad",
+    "url": "/cart-final/index.html"
+  },
+  {
+    "revision": "576ac9d75180b4c7f8f4",
+    "url": "/cart-final/static/css/main.1963ac7d.chunk.css"
+  },
+  {
+    "revision": "df6fac6b56ac4c876e79",
+    "url": "/cart-final/static/js/2.526b42e7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cart-final/static/js/2.526b42e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "576ac9d75180b4c7f8f4",
+    "url": "/cart-final/static/js/main.a90a71a4.chunk.js"
+  },
+  {
+    "revision": "875e05430a589fb57eb6",
+    "url": "/cart-final/static/js/runtime-main.4db1767d.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import Cart from './Cart';
+import Navbar from './Navbar';
+
+class App extends React.Component {
+
+  constructor () {
+    super();
+    this.state = {
+      products: [
+        {
+          price1: 99,
+          price:'Help Others',
+          title: 'Kindness',
+          qty: 1,
+          img: 'https://images.unsplash.com/photo-1518398046578-8cca57782e17?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+          id: 1
+        },
+        {
+          price1: 999,
+          price:'Empathize with them',
+          title: 'Empathy',
+          qty: 10,
+          img: 'https://images.unsplash.com/photo-1505243542579-da5adfe8338f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+          id: 2
+        },
+        {
+          price1: 999,
+          price:'To do whatever it takes',
+          title: 'Courage',
+          qty: 4,
+          img: 'https://images.unsplash.com/photo-1491225063580-7e097e87dcb2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80',
+          id: 3
+        }
+      ]
+    }
+    // this.increaseQuantity = this.increaseQuantity.bind(this);
+    // this.testing();
+  }
+  handleIncreaseQuantity = (product) => {
+    console.log('Heyy please inc the qty of ', product);
+    const { products } = this.state;
+    const index = products.indexOf(product);
+
+    products[index].qty += 1;
+
+    this.setState({
+      products
+    })
+  }
+  handleDecreaseQuantity = (product) => {
+    console.log('Heyy please inc the qty of ', product);
+    const { products } = this.state;
+    const index = products.indexOf(product);
+
+    if (products[index].qty === 0) {
+      return;
+    }
+
+    products[index].qty -= 1;
+
+    this.setState({
+      products
+    })
+  }
+  handleDeleteProduct = (id) => {
+    const { products } = this.state;
+
+    const items = products.filter((item) => item.id !== id); // [{}]
+
+    this.setState({
+      products: items
+    })
+  }
+
+  getCartCount = () => {
+    const { products } = this.state;
+
+    let count = 0;
+
+    products.forEach((product) => {
+      count += product.qty;
+    })
+
+    return count;
+  }
+
+  getCartTotal = () => {
+    const { products } = this.state;
+
+    let cartTotal = 0;
+
+    products.map((product) => {
+      cartTotal = cartTotal + product.qty * product.price1
+    })
+
+    return cartTotal;
+  }
+  render () {
+    const { products } = this.state;
+    return (
+      <div className="App">
+        <Navbar count={this.getCartCount()} />
+        <Cart
+          products={products}
+          onIncreaseQuantity={this.handleIncreaseQuantity}
+          onDecreaseQuantity={this.handleDecreaseQuantity}
+          onDeleteProduct={this.handleDeleteProduct}
+        />
+        <div style={ {padding: 10, fontSize: 20} }>TOTAL: {this.getCartTotal()} (Yup!!!They cost something) </div>
+      </div>
+    );
+  }
+}
+
+export default App;
